e4287b1157d28d49d17de9da3dc15de770d601b9
Gabriel Maciel <gabrielmaciel2310@gmail.com>
2020-07-06 16:15:40 -0400

    Initial Commit

---
+++ launchapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  launchapp
+//
+//  Created by Gabriel de Oliveira Maciel on 06/07/20.
+//  Copyright © 2020 Gabriel de Oliveira Maciel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
